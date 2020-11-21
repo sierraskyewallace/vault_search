ca7336e230e9d7b72daf0795448fa0862e4392c7
Sierra Wallace <sskwallace@gmail.com>
2020-11-21 01:15:36 +0000

    initial commit

---
+++ lib/vault_search.rb
@@ -0,0 +1,6 @@
+require "vault_search/version"
+
+module VaultSearch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vault_search/version.rb
@@ -0,0 +1,3 @@
+module VaultSearch
+  VERSION = "0.1.0"
+end
