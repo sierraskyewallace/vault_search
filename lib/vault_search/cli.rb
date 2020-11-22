74842a7bd787da60e59e32899d09c3fee062ef8d
Sierra Wallace <sskwallace@gmail.com>
2020-11-22 22:54:51 +0000

    adding menu options in CLI

--- lib/vault_search/cli.rb
+++ lib/vault_search/cli.rb
@@ -17,12 +17,24 @@ end
     if input == "list" 
       @concerts = VaultSearch::Vault 
       @vaults.each_with_index(1) do |vault, i|
-        puts "#{i}. #{vault.name} - #{vault.location} - #{vault.appearances} - #{vault.status}"
+        puts "#{i}. #{vault.name}"
+        elsif input == "exit" 
+        goodbye
+        exit
+      else
+        "Please type list to see a list of vaults or type exit to leave."
+        list_vaults
       end
     end
   end
   
   def menu
+    #fancy up
+    puts "Please select the number of the vault you'd like to learn more information about:"
+    input = gets.strip 
+    if input.to_i > 0 
+      vault = VaultSearch::Vault.find_by_index(input.to_i - 1)
+    puts "#{vault.name} - #{vault.location} - #{vault.appearances} - #{vault.status}"
     
 end
 
