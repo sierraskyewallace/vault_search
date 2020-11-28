004db6ec0af4628fa43403aaa53cf19bbbc88371
Sierra Wallace <sskwallace@gmail.com>
2020-11-28 02:32:21 +0000

    creating scraper outline

--- lib/vault_search/scraper.rb
+++ lib/vault_search/scraper.rb
@@ -1,4 +1,21 @@
 class VaultSearch::Scraper 
+  def self.scrape 
+    self.scrape_vaults
+  end
   
+  def self.scrape_vaults
+    vaults = [] 
+    vaults << self.scrape_fandom
+    vaults
+  end
+  
+  def self.scrape_fandom
+    doc = Nokogiri::HTML(open("https://fallout.fandom.com/wiki/List_of_Vaults"))
+    vault = self.new 
+    vault.name = 
+    vault.location = 
+    vault.status = 
+    vault.appearances = 
+  end
 end
 
