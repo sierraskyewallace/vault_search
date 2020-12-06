913d92cd941b0bd1a9829267bc1a09f324261aeb
Sierra Wallace <sskwallace@gmail.com>
2020-12-06 07:03:06 +0000

    refactored scraper method to only parse the first table of html doc

--- lib/vault_search/scraper.rb
+++ lib/vault_search/scraper.rb
@@ -5,7 +5,8 @@ class VaultSearch::Scraper
  def self.scrape_vaults
     vaults = []
     doc = Nokogiri::HTML(URI.open("https://fallout-archive.fandom.com/wiki/List_of_known_Vaults"))
-    rows = doc.search("tbody tr")
+    doc = doc.at('table')
+    rows = doc.search('tbody tr')
     rows.each do |row|
       vault = self.new
       vault.name =  row.search('td').map(&:text)[0]
