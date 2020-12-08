beaa9c1f4ae4f50025c00ccd53a43b15fb02cc55
Sierra Wallace <sskwallace@gmail.com>
2020-12-08 04:29:04 +0000

    after video demo and final test run

--- lib/vault_search/scraper.rb
+++ lib/vault_search/scraper.rb
@@ -4,15 +4,15 @@ class VaultSearch::Scraper
   
  def self.scrape_vaults
     vaults = []
-    doc = Nokogiri::HTML(URI.open("https://fallout-archive.fandom.com/wiki/List_of_known_Vaults"))
+    doc = Nokogiri::HTML(URI.open("https://fallout.fandom.com/wiki/List_of_Vaults?veaction=edit&section=1"))
     rows = doc.at('table')
     rows = doc.search('tbody tr:not(:first-child)')
     rows.each do |row|
       vault = self.new
       vault.name =  row.search('td').map(&:text)[0]
-      vault.location = row.search('td').map(&:text)[2]
+      vault.location = row.search('td').map(&:text)[3]
       vault.description = row.search('td').map(&:text)[1]
-      vault.status = row.search('td').map(&:text)[3]
+      vault.status = row.search('td').map(&:text)[2]
       vault.appearances = row.search('td').map(&:text)[4]
       vaults << vault
     end
