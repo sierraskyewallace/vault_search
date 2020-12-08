b82efcfc689795f69e22241bde36e205e87030d7
Sierra Wallace <sskwallace@gmail.com>
2020-12-08 00:45:22 +0000

    added color to cli text

--- lib/vault_search/cli.rb
+++ lib/vault_search/cli.rb
@@ -1,4 +1,4 @@
-
+require 'colorize'
 class VaultSearch::CLI
 
   def call
@@ -7,7 +7,9 @@ class VaultSearch::CLI
     goodbye
   end
   def list_vaults
-    puts "Welcome to Vault Search! Please type the number of the vault you'd like more information about or type exit to leave:"
+    puts "****************************************************************************".blue
+    puts "Welcome to Vault Search! Please type the number of the vault you'd like more information about or type exit to leave:".green
+    puts "****************************************************************************".blue
     @vaults = VaultSearch::Scraper.scrape_vaults
     @vaults.each.with_index(1) do |vault, i|
       puts "#{i}. #{vault.name}"
@@ -17,15 +19,20 @@ class VaultSearch::CLI
   def menu
       input = nil
     while input != "exit"
-      puts "Please select the vault you'd like to see more information about. To leave the program, type exit:"
+      puts "Please select the vault you'd like to see more information about. To leave the program, type exit:".green
       input = gets.strip.downcase
       if input.to_i > 0
         the_vault = @vaults[input.to_i - 1]
-        puts "Name: #{the_vault.name}"
-        puts "Description: #{the_vault.description}"
-        puts "Status: #{the_vault.status}"
-        puts "Location: #{the_vault.location}"
-        puts "Appearances: #{the_vault.appearances}"
+        puts "Name:".light_blue
+        puts "#{the_vault.name}"
+        puts "Description:".light_blue
+        puts "#{the_vault.description}"
+        puts "Status:".light_blue
+        puts "#{the_vault.status}"
+        puts "Location:".light_blue
+        puts "#{the_vault.location}"
+        puts "Appearances:".light_blue
+        puts " #{the_vault.appearances}"
         
       elsif input == "list"
         list_vaults
@@ -34,7 +41,7 @@ class VaultSearch::CLI
   
 
   def goodbye
-    puts "Have a great nuclear winter!"
+    puts "Have a great nuclear winter!".green
   end
 end
 end
